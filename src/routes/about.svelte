e3ca6916af10d89c3919252a60422bccdf4942d2
Sayan Mallick <snyxmk@gmail.com>
2021-11-05 20:03:25 +0530

    Data fetching setup using svelte store

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About the Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>Svelte Kit Pokedex</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { pokemon } from '../stores/pokeStore';
+</script>
+
+<svelte:head>
+	<title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>Pokedex</h1>
+
+{#each $pokemon as poke}
+	<p>{poke.name}</p>
+{/each}
