a33dbf0e537045521ed39f2c59b99be564da1ee1
Sayan Mallick <snyxmk@gmail.com>
2021-11-05 20:29:43 +0530

    Pokedex Completed

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,5 +2,13 @@
 	import Nav from '../components/nav.svelte';
 </script>
 
-<Nav />
-<slot />
+<dir class="p-8 max-w-6xl mx-auto">
+	<Nav />
+	<slot />
+</dir>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
--- src/stores/pokeStore.ts
+++ src/stores/pokeStore.ts
@@ -1,21 +1,36 @@
 import { writable } from 'svelte/store';
 
 export const pokemon = writable([]);
+const pokemonDetails = {};
+let loaded = false;
 
-const fetchPokemon = async () => {
-	const url = `https://pokeapi.co/api/v2/pokemon/?limit=100`;
-	const response = await fetch(url);
-	const data = await response.json();
-	const loadedPokemon = data.results.map((data, index) => {
-		return {
-			name: data.name,
-			id: index + 1,
-			image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-				index + 1
-			}.png`
-		};
-	});
+export const fetchPokemon = async () => {
+	if (loaded) return;
+	const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+	const res = await fetch(url);
+	const data = await res.json();
+	const loadedPokemon = data.results.map((data, index) => ({
+		name: data.name,
+		id: index + 1,
+		image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+			index + 1
+		}.png`
+	}));
 	pokemon.set(loadedPokemon);
+	loaded = true;
 };
 
-fetchPokemon();
+export const getPokemonById = async (id: number): Promise<any> => {
+	if (pokemonDetails[id]) return pokemonDetails[id];
+
+	try {
+		const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+		const res = await fetch(url);
+		const data = await res.json();
+		pokemonDetails[id] = data;
+		return data;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+};
